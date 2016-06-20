0d18283b81b4a886f1d51e19f0f1c9b44a304a86
shawnpoon <Shawn-PC>
2016-06-20 17:37:42 +0800

    bug fix

--- src/main/java/com/shawn/server/core/shiro/AccountManager.java
+++ src/main/java/com/shawn/server/core/shiro/AccountManager.java
@@ -1,7 +1,9 @@
 package com.shawn.server.core.shiro;
 
-public interface AccountManager {
+public interface AccountManager<T extends AuthenticationUser> {
 
-	public AuthenticationUser findByUsername(String username);
+	public boolean login(String username, String password);
+
+	public T findByUsername(String username);
 
 }
