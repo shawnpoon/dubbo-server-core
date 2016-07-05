e9cd983b160ae30cc1250cec1bff6628c0d42d29
shawnpoon <Shawn-PC>
2016-07-05 16:48:14 +0800

    update

--- src/main/java/com/shawn/server/core/shiro/AccountManager.java
+++ src/main/java/com/shawn/server/core/shiro/AccountManager.java
@@ -2,8 +2,6 @@ package com.shawn.server.core.shiro;
 
 public interface AccountManager<T extends AuthenticationUser> {
 
-	public boolean login(String username, String password);
-
 	public T findByUsername(String username);
 
 }
