192447d9f926fa947d917ba7806da915d9d73288
Shawn Poon <shawn@joosurenidemini>
2016-08-29 18:01:02 +0800

    update

---
+++ src/main/java/com/shawn/server/core/exception/AuthSignatureException.java
@@ -0,0 +1,7 @@
+package com.shawn.server.core.exception;
+
+public class AuthSignatureException extends Exception {
+
+	private static final long serialVersionUID = 1281767555551410418L;
+
+}
---
+++ src/main/java/com/shawn/server/core/exception/IllgalRequestParamException.java
@@ -0,0 +1,7 @@
+package com.shawn.server.core.exception;
+
+public class IllgalRequestParamException extends NumberFormatException {
+
+	private static final long serialVersionUID = 1844870446870778649L;
+
+}
