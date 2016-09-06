c2a45ef6c63e99888fd9ab74f56a58d2fa9c64eb
Shawnpoon <shawnpoon@qq.com>
2016-09-06 22:48:42 +0800

    update

--- src/main/java/com/shawn/server/core/shiro/AccountManager.java
+++ src/main/java/com/shawn/server/core/shiro/AccountManager.java
@@ -1,7 +1,15 @@
 package com.shawn.server.core.shiro;
 
+import org.apache.shiro.authc.ExcessiveAttemptsException;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.LockedAccountException;
+import org.apache.shiro.authc.UnknownAccountException;
+
 public interface AccountManager<T extends AuthenticationUser> {
 
 	public T findByUsername(String username);
+
+	boolean login(String username, String password) throws UnknownAccountException, ExcessiveAttemptsException,
+			LockedAccountException, IncorrectCredentialsException;
 
 }
